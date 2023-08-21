9a5bde39dbb436b82092fefcaa652e00610c3c8a
ermessias <ermessias@uolinc.com>
2023-08-21 17:35:42 -0300

    Aplicando o O do solid para reduzir o acoplamento

--- src/com/messias/service/ReajusteService.java
+++ src/com/messias/service/ReajusteService.java
@@ -1,32 +1,22 @@
 package com.messias.service;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
+import java.util.List;
 
 import com.messias.model.Funcionario;
-import com.messias.model.ValidacaoException;
 
 public class ReajusteService {
 	
+	private List<ValidacaoReajuste> validacaoReajustes;
+	
+	public ReajusteService(List<ValidacaoReajuste> validacaoReajustes) {
+		this.validacaoReajustes = validacaoReajustes;
+	}
+	
 	public void reajustarSalario(Funcionario funcionario ,BigDecimal aumento) {
-		BigDecimal percentualReajuste = aumento.divide(funcionario.getSalario(), RoundingMode.HALF_UP);
-		if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
-			throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
-		}
-		
-		LocalDate dataUltimoReajuste = funcionario.getDataUltimoReajuste();
-		LocalDate dataAtual = LocalDate.now();
-		
-		long mesesDesdeUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste, dataAtual);
-		
-		if(mesesDesdeUltimoReajuste < 6) {
-			throw new ValidacaoException("Intervalo entre reajustes deve ser de no minimo 6 meses");
-		}
-		
+	
+		validacaoReajustes.forEach(v -> v.validar(funcionario, aumento));
 		BigDecimal novoSalario = funcionario.getSalario().add(aumento);
-		
 		funcionario.AtualizarSalario(novoSalario);
 		
 	}
